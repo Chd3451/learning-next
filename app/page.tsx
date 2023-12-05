c1eba3b2d670418c75f9e38debab4368fc2353ad
Enmanuel Sánchez Abarúa <enmanuel.sanchez.a@gmail.com>
2023-12-04 20:21:18 -0400

    feat: add atomic design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import styles from '@/app/ui/home.module.css';
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
+import AcmeLogo from "./ui/atoms/acme-logo";
 
 export default function Page() {
   return (
